6c3552c322e782adcbb01f3f3fa3c6d467eee5a2
Parthifun <parthifunreddy29@gmail.com>
2025-09-30 18:22:22 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "TV" {
+  ami           = "ami-08982f1c5bf93d976"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Channel123"
+  }
+}
